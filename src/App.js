7bf38d0d902e35a78a4df38d501a2e6cf518eaef
abhay <kadamabhay40@gmail.com>
2023-01-30 21:42:39 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,60 +1,70 @@
-import React from 'react';
-import Header from './Tesla/Header';
-import Content from './Tesla/Content';
-import Footer from './Tesla/Footer';
+import React from "react";
+import Header from "./Tesla/Header";
+import Content from "./Tesla/Content";
+import Footer from "./Tesla/Footer";
 
 function App() {
   return (
-
     <>
+      <Header />
 
-    <Header />
+      <Content
+        carImg="model-3.jpg"
+        title="Model S"
+        discription="Schedule a Demo Drive"
+        leftBtn="Custom Order"
+        rightBtn="View Inventory"
+      />
 
-    <Content carImg = "model-3.jpg"
-    title = "Model S"
-    discription = "Schedule a Demo Drive"
-    leftBtn = "Custom Order"
-    rightBtn = "View Inventory" />
+      <Content
+        carImg="model-s.jpg"
+        title="Model 3"
+        discription="Schedule a Demo Drive"
+        leftBtn="Custom Order"
+        rightBtn="View Inventory"
+      />
 
-    <Content carImg = "model-s.jpg"
-    title = "Model 3"
-    discription = "Schedule a Demo Drive"
-    leftBtn = "Custom Order"
-    rightBtn = "View Inventory" />
+      <Content
+        carImg="model-x.jpg"
+        title="Model X"
+        discription="Schedule a Demo Drive"
+        leftBtn="Custom Order"
+        rightBtn="View Inventory"
+      />
 
-    <Content carImg = "model-x.jpg"
-    title = "Model X"
-    discription = "Schedule a Demo Drive"
-    leftBtn = "Custom Order"
-    rightBtn = "View Inventory" />
+      <Content
+        carImg="model-y.jpg"
+        title="Model Y"
+        discription="Schedule a Demo Drive"
+        leftBtn="Custom Order"
+        rightBtn="View Inventory"
+      />
 
-    <Content carImg = "model-y.jpg"
-    title = "Model Y"
-    discription = "Schedule a Demo Drive"
-    leftBtn = "Custom Order"
-    rightBtn = "View Inventory" />
+      <Content
+        carImg="solar-panel.jpg"
+        title="Solar Panels"
+        discription="Lowest Cost Solar Panels in America "
+        leftBtn="Custom Order"
+        rightBtn="View Inventory"
+      />
 
-    <Content carImg = "solar-panel.jpg"
-    title = "Solar Panels"
-    discription = "Lowest Cost Solar Panels in America "
-    leftBtn = "Custom Order"
-    rightBtn = "View Inventory" />
+      <Content
+        carImg="solar-roof.jpg"
+        title="Solar Roof"
+        discription="Produce Clear Energy From Your Roof"
+        leftBtn="Custom Order"
+        rightBtn="View Inventory"
+      />
 
-    <Content carImg = "solar-roof.jpg"
-    title = "Solar Roof"
-    discription = "Produce Clear Energy From Your Roof"
-    leftBtn = "Custom Order"
-    rightBtn = "View Inventory" />
+      <Content
+        carImg="accessories.jpg"
+        title="Accessories"
+        leftBtn="Shop Now"
+      />
 
-    <Content carImg = "accessories.jpg"
-    title = "Accessories"
-    leftBtn = "Shop Now" />
-
-    <Footer />
-
+      <Footer />
     </>
-
-  )
+  );
 }
 
 export default App;
--- src/Tesla/Header.js
+++ src/Tesla/Header.js
@@ -1,62 +1,98 @@
-import React, { useState } from 'react'
-import CloseIcon from '@mui/icons-material/Close';
-import styled from 'styled-components';
-import logo from "../tesla_images/logo.svg"
+import React, { useState } from "react";
+import CloseIcon from "@mui/icons-material/Close";
+import styled from "styled-components";
 
 function Header() {
-
   const [menuState, setMenuState] = useState(false);
 
   function ShowMenu(e) {
     e.preventDefault();
-    setMenuState(true)
+    setMenuState(true);
   }
 
   function HideMenu() {
-    setMenuState(false)
+    setMenuState(false);
   }
 
   return (
     <header>
       <nav>
-
-        <div><img className='logo' src='../tesla_images/logo.svg' alt='logo-img'></img></div>
-        <ul className='models'>
-          <li><a href=' '>Model S </a></li>
-          <li><a href=' '>Model X </a></li>
-          <li><a href=' '>Model Y </a></li>
-          <li><a href=' '>Model 3 </a></li>
-          <li><a href=' '>Solar Roof </a></li>
-          <li><a href=' '>Solar Panels </a></li>
+        <div>
+          <img className="logo" src="../images/logo.svg" alt="logo-img"></img>
+        </div>
+        <ul className="models">
+          <li>
+            <a href=" ">Model S </a>
+          </li>
+          <li>
+            <a href=" ">Model X </a>
+          </li>
+          <li>
+            <a href=" ">Model Y </a>
+          </li>
+          <li>
+            <a href=" ">Model 3 </a>
+          </li>
+          <li>
+            <a href=" ">Solar Roof </a>
+          </li>
+          <li>
+            <a href=" ">Solar Panels </a>
+          </li>
         </ul>
 
-        <ul className='about'>
-          <li><a href=' '>Shop</a></li>
-          <li><a href=' '>Account</a></li>
-          <li onClick={(e) => ShowMenu(e, 'inc')}><a href=' '>Menu</a></li>
+        <ul className="about">
+          <li>
+            <a href=" ">Shop</a>
+          </li>
+          <li>
+            <a href=" ">Account</a>
+          </li>
+          <li onClick={(e) => ShowMenu(e, "inc")}>
+            <a href=" ">Menu</a>
+          </li>
         </ul>
 
-
-        <Sidebar menuOpen={(menuState === false ? false : true)}>
-
+        <Sidebar menuOpen={menuState === false ? false : true}>
           <Closer onClick={HideMenu} />
 
-          <li><a href=' '>Esisting Inventory</a></li>
-          <li><a href=' '>Used Inventory</a></li>
-          <li><a href=' '>Trade-in</a></li>
-          <li><a href=' '>Demo Drive</a></li>
-          <li><a href=' '>Insurance</a></li>
-          <li><a href=' '>Roadster</a></li>
-          <li><a href=' '>Semi</a></li>
-          <li><a href=' '>Charging</a></li>
-          <li><a href=' '>Power wall</a></li>
-          <li><a href=' '>Find Us</a></li>
-          <li><a href=' '>Investor Relations</a></li>
+          <li>
+            <a href=" ">Esisting Inventory</a>
+          </li>
+          <li>
+            <a href=" ">Used Inventory</a>
+          </li>
+          <li>
+            <a href=" ">Trade-in</a>
+          </li>
+          <li>
+            <a href=" ">Demo Drive</a>
+          </li>
+          <li>
+            <a href=" ">Insurance</a>
+          </li>
+          <li>
+            <a href=" ">Roadster</a>
+          </li>
+          <li>
+            <a href=" ">Semi</a>
+          </li>
+          <li>
+            <a href=" ">Charging</a>
+          </li>
+          <li>
+            <a href=" ">Power wall</a>
+          </li>
+          <li>
+            <a href=" ">Find Us</a>
+          </li>
+          <li>
+            <a href=" ">Investor Relations</a>
+          </li>
         </Sidebar>
-
       </nav>
     </header>
-  )
+  );
 }
 
 const Sidebar = styled.ul`
@@ -64,37 +100,31 @@ const Sidebar = styled.ul`
   width: 230px;
   position: fixed;
   top: -20px;
-  right: ${(props) => props.menuOpen === false ? '-270px' : 0};
+  right: ${(props) => (props.menuOpen === false ? "-270px" : 0)};
   color: black;
   background-color: rgba(250, 250, 250, 0.97);
   padding-top: 30px;
-  list-style-type: none; 
+  list-style-type: none;
   transition: all 0.5s ease-in-out;
 
-  
   li {
-  margin: 30px 0;
-  padding: 0;
+    margin: 30px 0;
+    padding: 0;
 
-
-  a {
-  text-decoration: none;
-   color: black;
-   display: inline-block
-  }  
+    a {
+      text-decoration: none;
+      color: black;
+      display: inline-block;
+    }
   }
-   
-
-`
+`;
 
 const Closer = styled(CloseIcon)`
-position: absolute;
-right: 25px;
-top: 20px;
-cursor: pointer;
-font-size: 2.3rem !important;
-`
+  position: absolute;
+  right: 25px;
+  top: 20px;
+  cursor: pointer;
+  font-size: 2.3rem !important;
+`;
 
-
-
-export default Header
+export default Header;
